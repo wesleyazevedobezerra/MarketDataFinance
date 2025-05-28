dcc0f69a42f759bad9e688e07a3fb79e406a4e31
wesleyazevedobezerra <wesleyazevedobezerra@gmail.com>
2025-05-28 17:40:34 -0300

    Adicionado comentario e novos arquivos

---
+++ MarketDataFinance.Application/Factories/ScreenshotFactory.cs
@@ -0,0 +1,24 @@
+﻿using MarketDataFinance.Application.ViewModels;
+using MarketDataFinance.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketDataFinance.Application.Factories
+{
+    public static class ScreenshotFactory
+    {
+
+        public static ScreenshotEntity SetEntityToViewModel(RootFinanceChartEntity entity)
+        {
+            return new ScreenshotEntity(entity.Chart.Result[0].Meta);
+
+
+
+
+        }
+
+    }
+}
---
+++ MarketDataFinance.Domain/Entities/ScreenshotEntity.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketDataFinance.Domain.Entities
+{
+    public class ScreenshotEntity
+    {
+        public ScreenshotEntity(MetaFinanceChartEntity meta)
+        {
+            this.OneHundredPercent = 100;
+            this.symbol = meta.Symbol;
+            this.MarketPrice =  meta.RegularMarketPrice;
+            this.ChartPreviousClose = meta.ChartPreviousClose;
+        } 
+        //Comentario
+
+        public double MarketPrice { get; set; }
+
+        public string symbol { get; set; }
+         
+        public string RegularMarketPrice {
+            get
+            {
+                if (this.symbol.Contains("BRL") || this.symbol.Contains("^IXIC"))
+                    return this.MarketPrice.ToString("N2").Replace(",", ".");
+                else
+                    return Math.Truncate(this.MarketPrice).ToString("N0").Replace(",", ".");
+            }
+        }
+
+        public double ChartPreviousClose { get; set; }
+
+        private double OneHundredPercent { get; set; }
+
+        public double Percentual 
+            => ((this.MarketPrice - this.ChartPreviousClose) / this.ChartPreviousClose) * OneHundredPercent;
+        
+        public string PercentString  
+            => ((this.Percentual < 0) ? (this.Percentual * -1) : this.Percentual).ToString("N2").Replace(",", ".");
+         
+        public string PercentClass 
+            => this.Percentual >= 0 ? "positivo" : "negativo";
+        
+        public string PercentPlusMinusClass 
+            => this.Percentual >= 0 ? "+" : "-";
+        
+    }
+}
